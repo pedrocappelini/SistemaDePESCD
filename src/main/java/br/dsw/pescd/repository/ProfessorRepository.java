546d098f4038029fc34f1edd3e612cd3065331e6
Caio <caioishii123@hotmail.com>
2026-06-05 18:31:09 -0300

    feat(S01): criar ProfessorRepository e SecretarioRepository

---
+++ src/main/java/br/dsw/pescd/repository/ProfessorRepository.java
@@ -0,0 +1,13 @@
+package br.dsw.pescd.repository;
+
+import br.dsw.pescd.domain.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+    List<Professor> findAll();
+}
